e670c5ed412f5d361bbe6d62aaea1293a0cf8e97
Maha-I-Hendawy <maha.i.hendawy@gmail.com>
2024-05-06 19:50:33 +0300

    second commit

--- app.py
+++ app.py
@@ -1,4 +1,4 @@
-from flask import Flask
+from flask import Flask, render_template
 
 
 
@@ -8,7 +8,7 @@ app = Flask(__name__)
 
 @app.route('/')
 def home():
-	return "Home Page"
+	return render_template("home.html")
 
 
 
---
+++ static/main.js
@@ -0,0 +1,9 @@
+function changeColor(){
+
+	let elements = document.getElementsByTagName("h1");
+
+	for(element of elements){
+
+		element.style.color = 'blue'
+	}
+}
